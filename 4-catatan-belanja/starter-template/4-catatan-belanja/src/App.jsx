488df0dfe609e4194b7a72f1aaa50e208dfc669d
sapanly <sapanly.js@gmail.com>
2023-08-22 17:34:50 +0700

    update form

--- 4-catatan-belanja/starter-template/4-catatan-belanja/src/App.jsx
+++ 4-catatan-belanja/starter-template/4-catatan-belanja/src/App.jsx
@@ -1,6 +1,35 @@
+import { useState } from "react"
+
+const groceryItems = [
+  {
+    id: 1,
+    name: "Kopi Bubuk",
+    quantity: 2,
+    checked: true,
+  },
+  {
+    id: 2,
+    name: "Gula Pasir",
+    quantity: 5,
+    checked: false,
+  },
+  {
+    id: 3,
+    name: "Air Mineral",
+    quantity: 3,
+    checked: false,
+  },
+  {
+    id: 4,
+    name: "Air Gulas",
+    quantity: 3,
+    checked: false,
+  },
+]
+
 export default function App() {
   return (
-    <div class="app">
+    <div className="app">
       <Header />
 
       <Form />
@@ -17,18 +46,35 @@ function Header() {
 }
 
 function Form() {
+  const [name, setName] = useState("")
+  const [quantity, setQuantity] = useState(0)
+
+  function handleSubmit(e) {
+    e.preventDefault()
+    if (!name) return
+
+    const newItem = { name, quantity, checked: false, id: Date.now() }
+
+    console.log(newItem)
+
+    setName("")
+    setQuantity(1)
+  }
+
+  const quantityNum = [...Array(15)].map((_, i) => (
+    <option value={i + 1} key={i + 1}>
+      {i + 1}
+    </option>
+  ))
+
   return (
-    <form class="add-form">
+    <form className="add-form" onSubmit={handleSubmit}>
       <h3>Hari ini belanja apa kita?</h3>
       <div>
-        <select>
-          <option value="1">1</option>
-          <option value="2">2</option>
-          <option value="3">3</option>
-          <option value="4">4</option>
-          <option value="5">5</option>
+        <select value={quantity} onChange={(e) => setQuantity(Number(e.target.value))}>
+          {quantityNum}
         </select>
-        <input type="text" placeholder="nama barang..." />
+        <input type="text" placeholder="nama barang..." value={name} onChange={(e) => setName(e.target.value)} />
       </div>
       <button>Tambah</button>
     </form>
@@ -38,27 +84,14 @@ function Form() {
 function GroceryList() {
   return (
     <>
-      <div class="list">
+      <div className="list">
         <ul>
-          <li>
-            <input type="checkbox" checked="true" />
-            {/* <span style="text-decoration: line-through;">1 Kopi</span> */}
-            <span style={{ textDecoration: "line-through" }}>1 Kopi</span>
-            <button>&times;</button>
-          </li>
-          <li>
-            <input type="checkbox" />
-            <span>5 Gula Pasir</span>
-            <button>&times;</button>
-          </li>
-          <li>
-            <input type="checkbox" />
-            <span>3 Air Mineral</span>
-            <button>&times;</button>
-          </li>
+          {groceryItems.map((item) => (
+            <Item item={item} key={item.id} />
+          ))}
         </ul>
       </div>
-      <div class="actions">
+      <div className="actions">
         <select>
           <option value="input">Urutkan berdasarkan urutan input</option>
           <option value="name">Urutkan berdasarkan nama barang</option>
@@ -70,6 +103,18 @@ function GroceryList() {
   )
 }
 
+function Item({ item }) {
+  return (
+    <li key={item.id}>
+      <input type="checkbox" />
+      <span style={item.checked ? { textDecoration: "line-through" } : {}}>
+        {item.quantity} {item.name}
+      </span>
+      <button>&times;</button>
+    </li>
+  )
+}
+
 function Footer() {
-  return <footer class="stats">Ada 10 barang di daftar belanjaan, 5 barang sudah dibeli (50%)</footer>
+  return <footer className="stats">Ada 10 barang di daftar belanjaan, 5 barang sudah dibeli (50%)</footer>
 }
