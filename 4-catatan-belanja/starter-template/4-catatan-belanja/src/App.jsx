3d3a1b5ba8473e56e4fe67a4314b5d2a00e83c18
sapanly <sapanly.js@gmail.com>
2023-08-23 20:14:03 +0700

    update item.length in footer

--- 4-catatan-belanja/starter-template/4-catatan-belanja/src/App.jsx
+++ 4-catatan-belanja/starter-template/4-catatan-belanja/src/App.jsx
@@ -162,21 +162,18 @@ function Item({ item, onDeleteItem, onToggleItem }) {
   )
 }
 
-function Footer(items) {
-  // ngakalin agar masuk ke  array items
-  items = items.items
-
+function Footer({ items }) {
   if (items.length === 0) return <footer className="stats">Daftar belanjaan masih kosong!</footer>
 
   const totalItems = items.length
   const checkedItems = items.filter((item) => item.checked).length
   const percentage = Math.round((checkedItems / totalItems) * 100)
 
-  // console.log(items)
+  console.log(items)
 
   return (
     <footer className="stats">
-      Ada {totalItems} barang di daftar belanjaan, {checkedItems} barang sudah dibeli ({percentage}%){" "}
+      Ada {totalItems} barang di daftar belanjaan, {checkedItems} barang sudah dibeli ({percentage}%)
     </footer>
   )
 }
